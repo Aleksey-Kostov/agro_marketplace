2d1ca2ead287c4ae322e615f3d8b880653b23b26
Aleksey Kostov <aleksey_kostov@abv.bg>
2025-01-13 10:42:30 +0200

    fix: bugs

--- staticfiles/style/JavaScript/HideErrorsMessages.js
+++
@@ -1,8 +0,0 @@
-setTimeout(function() {
-            const alerts = document.querySelectorAll('.alert');
-            alerts.forEach(alert => {
-                alert.classList.remove('show');
-                alert.classList.add('fade');
-                alert.style.display = 'none';
-            });
-        }, 5000);
