f08813fb28fb899e8b5b9c08b3b41c8d70ef18dd
Aleksey Kostov <aleksey_kostov@abv.bg>
2025-05-09 01:00:39 +0300

    fix: fix deploy settings

--- agro_marketplace/core/storage_backends.py
+++ agro_marketplace/core/storage_backends.py
@@ -5,12 +5,12 @@ from storages.backends.azure_storage import AzureStorage
 class StaticAzureStorage(AzureStorage):
     account_name = os.getenv('AZURE_ACCOUNT_NAME')
     account_key = os.getenv('AZURE_ACCOUNT_KEY')
-    azure_container = os.getenv('AZURE_CONTAINER')
+    container_name = os.getenv('AZURE_CONTAINER')  # Corrected here
     expiration_secs = None
 
 
 class MediaAzureStorage(AzureStorage):
     account_name = os.getenv('AZURE_ACCOUNT_NAME')
     account_key = os.getenv('AZURE_ACCOUNT_KEY')
-    azure_container = os.getenv('AZURE_MEDIA_CONTAINER')
+    container_name = os.getenv('AZURE_MEDIA_CONTAINER')  # Corrected here
     expiration_secs = None
