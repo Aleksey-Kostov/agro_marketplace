a97e098b21c436892697d8ef2cdcdd261c159cfb
Aleksey Kostov <aleksey_kostov@abv.bg>
2025-05-08 18:51:08 +0300

    fix: fix deploy settings

--- agro_marketplace/settings.py
+++ agro_marketplace/settings.py
@@ -139,8 +139,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
