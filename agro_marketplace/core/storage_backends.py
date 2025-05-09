dab13c8b99cdf3de42419820318fd263a33d5738
Aleksey Kostov <aleksey_kostov@abv.bg>
2025-05-09 16:05:18 +0300

    fix: fix deploy settings

--- agro_marketplace/core/storage_backends.py
+++ agro_marketplace/core/storage_backends.py
@@ -1,17 +1,24 @@
-from storages.backends.azure_storage import AzureStorage
 import os
+from storages.backends.azure_storage import AzureStorage
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_var(name):
+    value = os.getenv(name)
+    if not value:
+        raise ImproperlyConfigured(f"Missing environment variable: {name}")
+    return value
 
 
 class StaticAzureStorage(AzureStorage):
-    account_name = os.getenv('AZURE_ACCOUNT_NAME')
-    account_key = os.getenv('AZURE_ACCOUNT_KEY')
-    azure_container = os.getenv('AZURE_CONTAINER')
+    account_name = get_env_var('AZURE_ACCOUNT_NAME')
+    account_key = get_env_var('AZURE_ACCOUNT_KEY')
+    azure_container = get_env_var('AZURE_CONTAINER')
     expiration_secs = None
 
 
 class MediaAzureStorage(AzureStorage):
-    account_name = os.getenv('AZURE_ACCOUNT_NAME')
-    account_key = os.getenv('AZURE_ACCOUNT_KEY')
-    azure_container = os.getenv('AZURE_MEDIA_CONTAINER')
+    account_name = get_env_var('AZURE_ACCOUNT_NAME')
+    account_key = get_env_var('AZURE_ACCOUNT_KEY')
+    azure_container = get_env_var('AZURE_MEDIA_CONTAINER')
     expiration_secs = None
-    file_overwrite = False  # prevents overwriting media with same name
