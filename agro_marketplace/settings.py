1b775b4c00388e68b5c13a022ac074080f5aa929
Aleksey Kostov <aleksey_kostov@abv.bg>
2025-05-10 11:47:05 +0300

    fix: fix deploy settings

--- agro_marketplace/settings.py
+++ agro_marketplace/settings.py
@@ -16,8 +16,6 @@ from decouple import config
 from django.urls import reverse_lazy
 import os
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
