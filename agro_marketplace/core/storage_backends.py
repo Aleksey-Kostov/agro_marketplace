43f54dd1fa4096585ef75ef3db273951069bce31
Aleksey Kostov <aleksey_kostov@abv.bg>
2025-05-10 18:43:27 +0300

    fix: fix deploy settings

--- agro_marketplace/core/storage_backends.py
+++ agro_marketplace/core/storage_backends.py
@@ -3,15 +3,16 @@ import os
 
 
 class StaticAzureStorage(AzureStorage):
-    account_name = os.getenv('AZURE_ACCOUNT_NAME')
-    account_key = os.getenv('AZURE_ACCOUNT_KEY')
-    azure_container = os.getenv('AZURE_CONTAINER', 'staticfiles')
+    account_name = os.environ.get('AZURE_ACCOUNT_NAME')
+    account_key = os.environ.get('AZURE_ACCOUNT_KEY')
+    azure_container = os.environ.get('AZURE_CONTAINER', 'staticfiles')
     expiration_secs = None
+    file_overwrite = True  # static files can be safely overwritten
 
 
 class MediaAzureStorage(AzureStorage):
-    account_name = os.getenv('AZURE_ACCOUNT_NAME')
-    account_key = os.getenv('AZURE_ACCOUNT_KEY')
-    azure_container = os.getenv('AZURE_MEDIA_CONTAINER', 'media')
+    account_name = os.environ.get('AZURE_ACCOUNT_NAME')
+    account_key = os.environ.get('AZURE_ACCOUNT_KEY')
+    azure_container = os.environ.get('AZURE_MEDIA_CONTAINER', 'media')
     expiration_secs = None
-    file_overwrite = False
+    file_overwrite = False  # media files usually should not be overwritten
