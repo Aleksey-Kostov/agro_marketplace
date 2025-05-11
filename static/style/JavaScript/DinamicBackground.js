5058bfebc487ae447ec0117f0ee90c5c1216bb6f
Aleksey Kostov <aleksey_kostov@abv.bg>
2025-05-11 16:10:00 +0300

    fix: fix deploy settings

--- static/style/JavaScript/DinamicBackground.js
+++ static/style/JavaScript/DinamicBackground.js
@@ -1,30 +1,38 @@
 const isProduction = window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1";
 
-const images = [
+const imageUrls = [
     isProduction
-        ? 'url("https://agromarket601d170f.blob.core.windows.net/static-content/images/texture.jpg")'
-        : 'url("/static/images/texture.jpg")',
+        ? "https://agromarket601d170f.blob.core.windows.net/static-content/images/texture.jpg"
+        : "/static/images/texture.jpg",
 
     isProduction
-        ? 'url("https://agromarket601d170f.blob.core.windows.net/static-content/images/pic1.jpg")'
-        : 'url("/static/images/pic1.jpg")',
+        ? "https://agromarket601d170f.blob.core.windows.net/static-content/images/pic1.jpg"
+        : "/static/images/pic1.jpg",
 
     isProduction
-        ? 'url("https://agromarket601d170f.blob.core.windows.net/static-content/images/pic2.jpg")'
-        : 'url("/static/images/pik2.jpg")',
+        ? "https://agromarket601d170f.blob.core.windows.net/static-content/images/pic2.jpg"
+        : "/static/images/pik2.jpg",
 
     isProduction
-        ? 'url("https://agromarket601d170f.blob.core.windows.net/static-content/images/pic3.jpg")'
-        : 'url("/static/images/pik3.jpg")',
+        ? "https://agromarket601d170f.blob.core.windows.net/static-content/images/pic3.jpg"
+        : "/static/images/pik3.jpg",
 
     isProduction
-        ? 'url("https://agromarket601d170f.blob.core.windows.net/static-content/images/pic4.jpg")'
-        : 'url("/static/images/pik4.jpg")'
+        ? "https://agromarket601d170f.blob.core.windows.net/static-content/images/pic4.jpg"
+        : "/static/images/pik4.jpg"
 ];
 
+// Preload images
+const preloadedImages = [];
+imageUrls.forEach((src) => {
+    const img = new Image();
+    img.src = src;
+    preloadedImages.push(img);
+});
+
 function setRandomBackground() {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    document.body.style.backgroundImage = images[randomIndex];
+    const randomIndex = Math.floor(Math.random() * imageUrls.length);
+    document.body.style.backgroundImage = `url("${imageUrls[randomIndex]}")`;
 }
 
 setRandomBackground();
