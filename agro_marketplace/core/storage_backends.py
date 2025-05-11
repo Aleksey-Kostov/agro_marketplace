46d06c63a2d23c7d1863033cf68596e39d04d7b6
Aleksey Kostov <aleksey_kostov@abv.bg>
2025-05-11 14:47:31 +0300

    fix: fix deploy settings

--- agro_marketplace/core/storage_backends.py
+++ agro_marketplace/core/storage_backends.py
@@ -1,18 +1,18 @@
 from storages.backends.azure_storage import AzureStorage
-import os
+from decouple import config
 
 
 class StaticAzureStorage(AzureStorage):
-    connection_string = os.getenv('AZURE_CONNECTION_STRING')
-    azure_container = os.getenv('AZURE_CONTAINER', 'static-content')
+    connection_string = config('AZURE_CONNECTION_STRING')
+    azure_container = config('AZURE_CONTAINER', default='static-content')
     expiration_secs = None
-    file_overwrite = True
-    location = ''
+    file_overwrite = True  # Overwrite files for static content
+    location = ''  # You can set a specific location within the container if needed
 
 
 class MediaAzureStorage(AzureStorage):
-    connection_string = os.getenv('AZURE_CONNECTION_STRING')
-    azure_container = os.getenv('AZURE_MEDIA_CONTAINER', 'media-content')
+    connection_string = config('AZURE_CONNECTION_STRING')
+    azure_container = config('AZURE_MEDIA_CONTAINER', default='media-content')
     expiration_secs = None
-    file_overwrite = False
-    location = ''
+    file_overwrite = False  # Don't overwrite media files
+    location = ''  # You can set a specific location within the container if needed
