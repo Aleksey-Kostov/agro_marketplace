a33321d7651e17c56d449fbb7332411ab876218c
Aleksey Kostov <133632139+Aleksey-Kostov@users.noreply.github.com>
2026-08-31 13:08:18 +0300

    Update DinamicBackground.js

--- static/style/JavaScript/DinamicBackground.js
+++ static/style/JavaScript/DinamicBackground.js
@@ -1,29 +1,14 @@
-const isProduction = window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1";
-
 const imageUrls = [
-    isProduction
-        ? "https://agromarket601d170f.blob.core.windows.net/static-content/images/texture.jpg"
-        : "/static/images/texture.jpg",
-
-    isProduction
-        ? "https://agromarket601d170f.blob.core.windows.net/static-content/images/pic1.jpg"
-        : "/static/images/pic1.jpg",
-
-    isProduction
-        ? "https://agromarket601d170f.blob.core.windows.net/static-content/images/pic2.jpg"
-        : "/static/images/pic2.jpg",
-
-    isProduction
-        ? "https://agromarket601d170f.blob.core.windows.net/static-content/images/pic3.jpg"
-        : "/static/images/pic3.jpg",
-
-    isProduction
-        ? "https://agromarket601d170f.blob.core.windows.net/static-content/images/pic4.jpeg"
-        : "/static/images/pic4.jpeg"
+    "/static/images/texture.jpg",
+    "/static/images/pic1.jpg",
+    "/static/images/pic2.jpg",
+    "/static/images/pic3.jpg",
+    "/static/images/pic4.jpeg"
 ];
 
 // Preload images
 const preloadedImages = [];
+
 imageUrls.forEach((src) => {
     const img = new Image();
     img.src = src;
@@ -32,8 +17,11 @@ imageUrls.forEach((src) => {
 
 function setRandomBackground() {
     const randomIndex = Math.floor(Math.random() * imageUrls.length);
-    document.body.style.backgroundImage = `url("${imageUrls[randomIndex]}")`;
+
+    document.body.style.backgroundImage =
+        `url("${imageUrls[randomIndex]}")`;
 }
 
 setRandomBackground();
+
 setInterval(setRandomBackground, 10000);
