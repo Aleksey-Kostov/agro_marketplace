7e7bb3dcb568328925c6221a26d989bb8f759164
Aleksey Kostov <aleksey_kostov@abv.bg>
2026-09-11 11:30:27 +0300

    fix: improve UI and fix bugs

--- static/style/JavaScript/conversation.js
+++ static/style/JavaScript/conversation.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ========================================================== */
 
     const chatWindow =
-        document.getElementById('chat-window', 'text-area');
+        document.getElementById('chat-window');
 
     const scrollTopBtn =
         document.getElementById('scroll-top-btn');
