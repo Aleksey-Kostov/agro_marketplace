c4bd1ba67e680c8b95d31788fd0f54dd58680a45
Aleksey Kostov <aleksey_kostov@abv.bg>
2026-09-24 23:44:36 +0300

    fix: improve UI and fix bugs

--- agro_marketplace/agro_messages/routing.py
+++ agro_marketplace/agro_messages/routing.py
@@ -5,7 +5,7 @@ from .consumers import MessageConsumer
 
 websocket_urlpatterns = [
     re_path(
-        r"ws/messages/(?P<message_id>\d+)/$",
+        r"ws/messages/(?P<pk>\d+)/$",
         MessageConsumer.as_asgi(),
     ),
 ]
